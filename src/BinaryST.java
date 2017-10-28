b7519c57b5f5104bea34db82532bf7660a2d27b4
B_Wilhelm <bwilhelm@iastate.edu>
2017-10-28 17:59:05 -0500

    initial commit containing template files

---
+++ src/BinaryST.java
@@ -0,0 +1,83 @@
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add additional methods and fields)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
+
+
+public class BinaryST
+{
+	
+	
+	public BinaryST()
+	{
+		// TODO
+	}
+	
+	public BinaryST(String[] s)
+	{
+		// TODO
+	}
+	
+	public int distinctSize()
+	{
+		// TODO
+		return 0;
+	}
+	
+	public int size()
+	{
+		// TODO
+		return 0;
+	}
+	
+	public int height()
+	{
+		// TODO
+		return 0;
+	}
+	
+	public void add(String s)
+	{
+		// TODO
+	}
+	
+	public boolean search(String s)
+	{
+		// TODO
+		return false;
+	}
+	
+	public int frequency(String s)
+	{
+		// TODO
+		return 0;
+	}
+	
+	public boolean remove(String s)
+	{
+		// TODO
+		return false;
+	}
+	
+	public String[] inOrder()
+	{
+		// TODO
+		return null;
+	}
+	
+	public String[] preOrder()
+	{
+		// TODO
+		return null;
+	}
+	
+	public int rankOf(String s)
+	{
+		// TODO
+		return 0;
+	}
+}
---
+++ src/WarWithHash.java
@@ -0,0 +1,28 @@
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add member fields and additional methods)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
+
+import java.util.ArrayList;
+
+
+public class WarWithHash
+{
+	
+	
+	public WarWithHash(String[] s, int k)
+	{
+		// TODO
+	}
+	
+	public ArrayList<String> compute2k()
+	{
+		// TODO
+		return null;
+	}
+}
+
