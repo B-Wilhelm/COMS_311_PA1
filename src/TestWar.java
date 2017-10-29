1c1cd235388626737fe2f2b46544c7bf6a80a6a2
B_Wilhelm <bwilhelm@iastate.edu>
2017-10-29 16:48:13 -0500

    Improved formatting of test output

--- src/TestWar.java
+++ src/TestWar.java
@@ -7,12 +7,12 @@ public class TestWar {
 		
 		WarWithArray wa = new WarWithArray(arr, arr[0].length());
 		start = System.nanoTime();
-		System.out.println(wa.compute2k().toString());
+		System.out.println("Array:\n" + wa.compute2k().toString());
 		System.out.println(System.nanoTime()-start);
 		
 		WarWithHash wh = new WarWithHash(arr, arr[0].length());
 		start = System.nanoTime();
-		System.out.println(wh.compute2k().toString());
+		System.out.println("\nHashtable:\n" + wh.compute2k().toString());
 		System.out.println(System.nanoTime()-start);
 	}
 }
