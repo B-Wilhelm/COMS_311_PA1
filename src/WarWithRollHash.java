c7c8714d715c73bfc0a8d86f411600ab9bc75e21
B_Wilhelm <bwilhelm@iastate.edu>
2017-10-29 22:30:57 -0500

    testing

--- src/WarWithRollHash.java
+++ src/WarWithRollHash.java
@@ -13,10 +13,12 @@
 	(i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
 */
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public class WarWithRollHash
@@ -25,23 +27,15 @@ public class WarWithRollHash
 	private int k;
 	private ArrayList<String> c;
 	private String temp;
-	private final Map<String, Integer> map = new HashMap<>();
-	private int A_SIZE_POW_9;
 	
 	/**
 	 * creates an object that stores an input string-array (into a Rolling Hash) of DNA sequences for later computation
 	 * @param	s	The set of substrings (length k) of the DNA sequence
 	 * @param	k	The length of substrings (in s) of the DNA sequence
 	 */
-	public WarWithRollHash(String[] s, int k)
-	{
+	public WarWithRollHash(String[] s, int k) {
 		this.s = s;
 		this.k = k;
-		
-		for(String value : s)
-			map.put(value, 0);
-		
-		A_SIZE_POW_9 = (int) Math.pow(map.size(), 9);
 	}
 	
 	/**
@@ -49,29 +43,30 @@ public class WarWithRollHash
 	 * @return		The ArrayList of all substrings of the DNA sequence with length 2*k
 	 */
 	public ArrayList<String> compute2k() {
-		temp = "";
-		c = new ArrayList<String>();
 		
-		for(int i = 0; i < s.length; i++) {
-			for(int j = 0; j < s.length; j++) {
-				temp = s[i]+s[j];
-				c.addAll(validCheck(temp));
-			}
-		}
-		
-		return c;
 	}
 	
-	private ArrayList<String> validCheck(String s) {	    
-		Set<String> set = new HashSet<>();
-        Set<Integer> hashes = new HashSet<>();
-        c = new ArrayList<String>();
-        
-        for (int i = 0, rhash = 0; i < s.length(); i++) {
-            if (i > 9) rhash -= A_SIZE_POW_9 * map.get(s.charAt(i-10));
-            rhash = map.size() * rhash + map.get(s.charAt(i));
-            if (i > 8 && !hashes.add(rhash)) c.add(s.substring(i-9,i+1));
-        }
-        return c;
+	private boolean validCheck(String P, String T, int q) {
+		int m = P.length();
+		int n = T.length();
+		int p = 0; // hash of pattern P
+		int t = 0; // hash of text T
+		int h = (d ^ (m-1)) % q;
+
+		// preprocessing: hashing P[1..m] and T[1..m] (first window of T)
+		for (int i = 1; i < m; i++) {
+		    p = (d * p + P[i]) % q; //(1)
+		    t = (d * t + T[i]) % q;
+		}
+
+		// matching
+		for (int j = 0; j < n-m; j++) {
+		    if(p == t) {
+		        if(P[1..m] == T[j+1..j+m]
+		            System.out.println("matched");
+		    }
+		    // update the rolling hash
+		    if(j < n-m)
+		        t = (d * (t - T[j+1] * h) + T[j+m+1]) % q;
+		}
 	}
-}
